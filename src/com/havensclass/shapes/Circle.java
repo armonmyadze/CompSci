5709f25d3d6055cb41026af9ab1c9e540f32486f
armon.myadze <armonmyadze@gmail.com>
2019-10-08 11:10:57 -0500

    Added shapes package

---
+++ src/com/havensclass/shapes/Circle.java
@@ -0,0 +1,16 @@
+package com.havensclass.shapes;
+
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.radius = radius;
+        this.position = center;
+    }
+
+    @Override
+    public double computeArea() {
+        return (Math.PI * Math.pow(radius, 2));
+    }
+}
